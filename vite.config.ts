f4655bb7ea17ef7d76cf3608762340f318875f0c
HOTPOT <tpcmaky@gmail.com>
2025-06-10 13:10:44 +0800

    fixed dep

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,11 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
   },
   optimizeDeps: {
     exclude: ["lucide-react"],
